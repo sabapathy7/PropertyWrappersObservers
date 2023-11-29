92ed195c98f654c4d62f9348b0596636a9ea259d
Kanagasabapathy Rajkumar <40764138+sabapathyk7@users.noreply.github.com>
2023-11-29 06:57:02 +0530

    Initial Commit

---
+++ PropertyWrappers/PropertyWrappersApp.swift
@@ -0,0 +1,17 @@
+//
+//  PropertyWrappersApp.swift
+//  PropertyWrappers
+//
+//  Created by kanagasabapathy on 29/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PropertyWrappersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
