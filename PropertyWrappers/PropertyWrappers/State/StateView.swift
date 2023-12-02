badbd77472c2b64af793685ef90b35e036444e5b
Kanagasabapathy <trksabapathy7@gmail.com>
2023-12-02 23:55:02 +0530

    @State Property Wrapper - SwiftUI View - Toggle between different colors - Single Source of truth of value types

---
+++ PropertyWrappers/PropertyWrappers/State/StateView.swift
@@ -0,0 +1,43 @@
+//
+//  StateView.swift
+//  PropertyWrappers
+//
+//  Created by kanagasabapathy on 29/11/23.
+//
+
+import SwiftUI
+
+struct StateView: View {
+    @State private var colorToggle: Color = .blue
+    var body: some View {
+        Text("@State").font(.title)
+        Text("Single source of truth for value types")
+            .font(.subheadline)
+
+        Spacer()
+
+        Rectangle()
+            .fill(colorToggle)
+            .frame(width: 100, height: 100)
+            .foregroundStyle(.foreground)
+            .clipShape(RoundedRectangle(cornerSize: CGSize(width: 20, height: 10)))
+
+        Text(colorToggle == .blue ? "Blue" : "Black")
+
+        Button() {
+            colorToggle = colorToggle == .black ? .blue : .black
+        } label: {
+            Text("Tap")
+        }
+        .padding()
+
+        Spacer()
+        
+        Text("@State owns and can mutate the property")
+            .font(.footnote)
+    }
+}
+
+#Preview {
+    StateView()
+}
